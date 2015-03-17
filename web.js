ab5de77c706384010798c96f768857da2198270f
amurdock <alasdair.murdock@mudaless.com>
2015-03-17 21:20:41 +0000

    test ... 2

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var port = process.env.PORT || 5000;
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello from DOKKU (BOOM) on port '+port+' from container '+process.env.HOSTNAME+'\n');
+  res.end('Hello from DOKKU (BOOM BOOM) on port '+port+' from container '+process.env.HOSTNAME+'\n');
 }).listen(port, function() {
   console.log("Listening on " + port);
 });
